d19aea0e531ddbe1cc2d539807d302c345c2e17f
Quaden <ldarksoull@yandex.ru>
2018-05-07 03:08:03 +0300

    Прикрутил скрипт моб меню

---
+++ js/jquery.mobile-offcanvas-menu.js
@@ -0,0 +1,125 @@
+/**
+ * created by @jaynoe
+ * https://github.com/jaynoe
+ *
+ * Plugin: offcanvas-mobile-menu
+ * https://github.com/jaynoe/offcanvas-mobile-menu
+*/
+;(function($){
+
+  $.fn.offcanvasmenu = function(options){
+
+    // Default options
+    options = $.extend({
+      menuTrigger: "trigger",
+      position: "right",
+      speed: "0.3",
+      width: "220px",
+      fixedContainer: false,
+      fixedContainerName: ""
+    }, options);
+
+    // Build the vars
+    // Containers:
+    canvas = $(this);
+    trigger = $("#" + options.menuTrigger);
+    body = $("body");
+    link = canvas.find("a");
+    pos = options.position;
+    fc = $("#" + options.fixedContainerName);
+
+    // inline styles
+    // for body
+    body.css({
+      "-webkit-transition": "all " + options.speed + "s ease-in-out",
+      "transition": "all " + options.speed + "s ease-in-out",
+      "position": "relative"
+    })
+    // for offcanvas:
+    canvas.css({
+      "-webkit-transition": "all " + options.speed + "s ease-in-out",
+      "transition": "all " + options.speed + "s ease-in-out",
+      "width": options.width,
+      "display": "block"
+    });
+    // for fixedContainer
+    if(options.fixedContainer == true) {
+    	fc.css({
+    		"-webkit-transition": "all " + options.speed + "s ease-in-out",
+    		"transition": "all " + options.speed + "s ease-in-out",
+    	})
+    }
+
+    // check position
+    if(pos == "right") {
+      body.css("left", "0");
+      canvas.css({
+        "right": "0",
+        "-webkit-transform": "translate(" + options.width + ", 0)",
+        "transform": "translate(" + options.width + ", 0)"
+      });
+      fc.css("left", "0");
+    } else {
+      body.css("right", "0");
+      canvas.css({
+        "left": "0",
+        "-webkit-transform": "translate(-" + options.width + ", 0)",
+        "transform": "translate(-" + options.width + ", 0)"
+      });
+      fc.css("right", "0");
+    }
+
+    // function out & hide
+    function canvasOut() {
+      canvas.css({
+        "-webkit-transform": "translate(0, 0)",
+        "transform": "translate(0, 0)"
+      });
+
+      if(pos == "right") { 
+      	body.css("left", "-" + options.width);
+      	fc.css("left", "-" + options.width);
+      }
+      else {
+      	body.css("right", "-" + options.width);
+      	fc.css("right", "-" + options.width);
+      	
+      }
+    }
+
+    function canvasHide() {
+
+      if(pos == "right") {
+        body.css("left", "0");
+        canvas.css({
+          "-webkit-transform": "translate(" + options.width + ", 0)",
+          "transform": "translate(" + options.width + ", 0)",
+        });
+        fc.css("left", "0");
+      } else {
+        body.css("right", "0");
+        canvas.css({
+          "left": "0",
+          "-webkit-transform": "translate(-" + options.width + ", 0)",
+          "transform": "translate(-" + options.width + ", 0)"
+        });
+        fc.css("right", "0");
+      }
+    }
+
+    // if click on the trigger
+    trigger.click(function() {
+      canvasOut();
+      clicks = $(this).data('clicks');
+      if(clicks) { canvasHide(); }
+      $(this).data("clicks", !clicks);
+    });
+
+    // close when click on link in menu
+    link.click(function() {
+      canvasHide();
+    });
+
+  };
+
+})(jQuery);
--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
-$(document).ready(function () {
-    $("#btn").click(function () {
-        $("#nav").slideToggle("slow");
-        $(this).toggleClass("active");
-    });
-});
+// Менюшка
+if($(window).width() < 568){
+$("#offcanvas").offcanvasmenu({
+    // trigger element
+    menuTrigger: "trigger",
+    // or 'left'
+    position: "left",
+    // animation speed
+    speed: "0.2",
+    // menu width
+    width: "200px"
+  });}
+// кнопка менюшки
+(function() {
+    "use strict";
+    var toggles = document.querySelectorAll(".cmn-toggle-switch");
+  
+    for (var i = toggles.length - 1; i >= 0; i--) {
+      var toggle = toggles[i];
+      toggleHandler(toggle);
+    };
+    function toggleHandler(toggle) {
+      toggle.addEventListener( "click", function(e) {
+        e.preventDefault();
+        (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+      });
+    }
+  })();
